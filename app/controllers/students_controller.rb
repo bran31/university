3cfa9e3f1c194aca282eac37a171a28df0c94909
bran31 <bran31@gmail.com>
2019-02-07 11:27:39 -0500

    added restrictions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
+    skip_before_action :require_user, only: [:new, :create]
     before_action :set_student, only: [:show, :edit, :update]
+    before_action :require_same_student, only: [:update, :edit]
 
     def index
         @student = Student.all
@@ -47,5 +49,10 @@ class StudentsController < ApplicationController
         params.require(:student).permit(:name, :email, :password, :password_confirmation)
     end
 
-
+    def require_same_student
+        if current_user != @student
+            redirect_to student_path(current_user)
+            flash[:notice] = "You can only edit your own profile!"
+        end
+    end
 end
